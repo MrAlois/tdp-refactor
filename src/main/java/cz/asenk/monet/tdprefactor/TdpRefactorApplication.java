c6aae61daba1d69d178a97fdf2827f6e640cf436
Senkyrik Alois <asenkyrik@monetplus.cz>
2023-03-13 14:18:10 +0100

    Initial commit

---
+++ src/main/java/cz/asenk/monet/tdprefactor/TdpRefactorApplication.java
@@ -0,0 +1,13 @@
+package cz.asenk.monet.tdprefactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdpRefactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdpRefactorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/asenk/monet/tdprefactor/TdpRefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.asenk.monet.tdprefactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdpRefactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
